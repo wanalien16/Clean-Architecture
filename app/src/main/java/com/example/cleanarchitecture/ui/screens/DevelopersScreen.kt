1f76f7cbc4f0f4642a65b42c6839a1eb49522189
anilsai <anilsai.damayapally16@gmail.com>
2024-09-11 19:48:03 +0530

    Added favourites button to the list of developers that fetched from the local database.

--- app/src/main/java/com/example/cleanarchitecture/ui/screens/DevelopersScreen.kt
+++ app/src/main/java/com/example/cleanarchitecture/ui/screens/DevelopersScreen.kt
@@ -10,17 +10,27 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.rememberAsyncImagePainter
@@ -42,8 +52,10 @@ Box(modifier = Modifier.fillMaxSize()){
 
 @Composable
 fun CardView(developer: Developers){
-Card(modifier = Modifier.fillMaxWidth()) {
-Row {
+Card(modifier = Modifier
+    .fillMaxWidth()
+    .padding(horizontal = 16.dp, vertical = 4.dp)) {
+Row(modifier = Modifier.padding(5.dp), verticalAlignment = Alignment.CenterVertically) {
     Image(painter = rememberAsyncImagePainter(developer.avatar ), contentDescription = "Developer Image",
         modifier = Modifier
             .size(60.dp)
@@ -51,18 +63,25 @@ Row {
             .padding(5.dp)
             .background(MaterialTheme.colorScheme.surface))
 
-    Spacer(modifier = Modifier.padding(20.dp))
+    Spacer(modifier = Modifier.width(5.dp))
 
-    Column(modifier = Modifier.padding(5.dp)) {
+    Column(modifier = Modifier.padding(5.dp).weight(1f)) {
 
 //        Text(text = developer.name)
         
-        Text(text = developer.username)
+        Text(text = developer.username, color = Color.Green)
         
-        Text(text = developer.url)
+        Text(text = developer.url, color = Color.Blue, maxLines = 1, overflow = TextOverflow.Ellipsis)
 
 
     }
+
+IconButton(onClick = { /*TODO*/ }) {
+    Icon(imageVector = if (developer.isFavourite) Icons.Default.Favorite
+    else Icons.Default.FavoriteBorder, contentDescription = "Add to favourites",
+        tint = if (developer.isFavourite) Color.Red else Color.Gray)
+}
+
 
 }
 }
