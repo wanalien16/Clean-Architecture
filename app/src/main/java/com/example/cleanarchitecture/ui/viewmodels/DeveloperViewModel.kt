301b63a42312ff5f905c7325a4781d1e727e329f
anilsai <anilsai.damayapally16@gmail.com>
2024-09-16 23:50:32 +0530

    implementing code to map local data with remote data

--- app/src/main/java/com/example/cleanarchitecture/ui/viewmodels/DeveloperViewModel.kt
+++ app/src/main/java/com/example/cleanarchitecture/ui/viewmodels/DeveloperViewModel.kt
@@ -4,7 +4,6 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.cleanarchitecture.domain.model.Developers
-import com.example.cleanarchitecture.domain.repository.DeveloperRepo
 import com.example.cleanarchitecture.domain.usecase.GetDeveloperUseCase
 import com.example.cleanarchitecture.domain.usecase.UpdateDeveloperFavouriteUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -16,13 +15,17 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class DeveloperViewModel @Inject constructor(private val getDeveloperUseCase: GetDeveloperUseCase,
-    private val updateDeveloperFavouriteUseCase: UpdateDeveloperFavouriteUseCase) :
+class DeveloperViewModel @Inject constructor(
+    private val getDeveloperUseCase: GetDeveloperUseCase,
+    private val updateDeveloperFavouriteUseCase: UpdateDeveloperFavouriteUseCase
+) :
     ViewModel() {
 
     private val _data = MutableStateFlow<List<Developers>>(emptyList())
     val data: StateFlow<List<Developers>> = _data.asStateFlow()
 
+    private val _favourite = MutableStateFlow(false)
+    var favourite: StateFlow<Boolean> = _favourite
 
 
     init {
@@ -35,15 +38,28 @@ class DeveloperViewModel @Inject constructor(private val getDeveloperUseCase: Ge
                 val response = getDeveloperUseCase()
                 Log.i("getting", "data $response")
                 viewModelScope.launch { _data.value = response }
-            }.getOrElse { Log.e("error is", it.localizedMessage)}
+            }.getOrElse { Log.e("error is", it.localizedMessage) }
 
         }
     }
 
-     fun toggleFavourite(username: String, isFavourite: Boolean){
-         viewModelScope.launch(Dispatchers.IO) { val newFavouriteStatus = !isFavourite
-         updateDeveloperFavouriteUseCase.invoke(username,newFavouriteStatus)}
+    fun toggleFavourite(username: String, isFavourite: Boolean?) {
+        viewModelScope.launch(Dispatchers.IO) {
 
+            if (isFavourite==null){
+                isFavourite = null
+            }
+            val developer =
+                data.value.find { it.username == username }?.copy(isFavourite = newFavouriteStatus)
+                    ?: return@launch
+            val developerPosition = data.value.indexOfFirst { it.username == username }.takeIf { it >= 0 } ?:return@launch
+            val newList = data.value.toMutableList()
+            newList.removeAt(developerPosition)
+            newList.add(developerPosition, developer)
+            _data.value = newList
+            updateDeveloperFavouriteUseCase(username, newFavouriteStatus)
+
+        }
 
 
     }
